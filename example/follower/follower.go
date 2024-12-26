3b6f0abda38bfc488cb7b8c7950fb65c4c2b8d98
zhuzongxin <dreamsxin@qq.com>
2024-12-26 08:05:02 +0000

    Add follower

---
+++ example/follower/follower.go
@@ -0,0 +1,94 @@
+//go:build exclude
+// +build exclude
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/dreamsxin/gonx"
+	"github.com/dreamsxin/gonx/follower"
+)
+
+var format string
+var logFile string
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.StringVar(&format, "format", `$remote_addr [$time_local] "$request" $status $request_length $body_bytes_sent $request_time "$t_size" $read_time $gen_time`, "Log format")
+	flag.StringVar(&logFile, "log", "access.log", "Log file name")
+}
+
+var (
+	locker sync.Mutex
+	logs   []string
+)
+
+func main() {
+
+	// Create a parser based on given format
+	parser := gonx.NewParser(format)
+
+	t, err := follower.New(logFile, follower.Config{
+		Whence: io.SeekStart,
+		Offset: 0,
+		Reopen: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	reducer := gonx.NewChain(
+		&gonx.Avg{Fields: map[string]string{"request_time": "request_time", "read_time": "read_time", "gen_time": "gen_time"}},
+		&gonx.Sum{Fields: map[string]string{"body_bytes_sent": "body_bytes_sent"}},
+		&gonx.Count{},
+	)
+	var wg sync.WaitGroup
+	var entries chan *gonx.Entry
+	var lasttime time.Time
+	for line := range t.Lines() {
+		//log.Println(line.String())
+		entry, err := parser.ParseString(line.String())
+		if err == nil {
+			time_local, err := entry.Field("time_local")
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			t, err := time.ParseInLocation("02/Jan/2006:15:04:05 -0700", time_local, time.Local)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if lasttime.Format("2006-01-02") != t.Format("2006-01-02") {
+				if entries != nil {
+					close(entries)
+				}
+
+				log.Println("start new day", t.Format("2006-01-02"))
+				entries = make(chan *gonx.Entry, 10)
+				wg.Add(1)
+				go func(t time.Time, entries chan *gonx.Entry) {
+					defer wg.Done()
+					var output = make(chan *gonx.Entry)
+					go reducer.Reduce(entries, output)
+					for res := range output {
+						// Process the record... e.g.
+						fmt.Printf("Parsed entry: %s, %+v\n", t.Format("2006-01-02"), res)
+					}
+
+				}(t, entries)
+			}
+			lasttime = t
+			entries <- entry
+		} else {
+			log.Println(err)
+		}
+	}
+	wg.Wait()
+}
--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/dreamsxin/gonx
 
 go 1.21
 
-require github.com/smartystreets/goconvey v1.7.2
+require (
+	github.com/fsnotify/fsnotify v1.8.0
+	github.com/smartystreets/goconvey v1.7.2
+)
 
 require (
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
